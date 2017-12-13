1b64bc4b1d09ff2e16d28cef5ea8f6caa4d5ccf3
chenjinle <jinlesoft@163.com>
2017-12-13 14:15:54 +0800

    first

--- mopian/src/main/java/com/rzico/assistant/wxapi/WXPayEntryActivity.java
+++ mopian/src/main/java/com/rzico/assistant/wxapi/WXPayEntryActivity.java
@@ -1,5 +1,9 @@
 package com.rzico.assistant.wxapi;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.rzico.weex.module.WXEventModule;
 import com.tencent.mm.sdk.constants.ConstantsAPI;
 import com.tencent.mm.sdk.modelbase.BaseReq;
@@ -7,12 +11,6 @@ import com.tencent.mm.sdk.modelbase.BaseResp;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
 
 public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
 
