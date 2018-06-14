cf3e175642e130833266f8a4c687ace2a72e7dd9
zhangsr <zhangsr@tiaohuo.com>
2018-06-14 11:06:37 +0800

    xmsdar

--- constant/src/main/java/com/rzico/weex/constant/AllConstant.java
+++ constant/src/main/java/com/rzico/weex/constant/AllConstant.java
@@ -11,7 +11,7 @@ import java.util.UUID;
  */
 
 public class AllConstant {
-    public static final String PACKNAME = "com.rzico.assistant";
+    public static final String PACKNAME = "com.rzico.pine";
 
     public static int isClearAll = 1;
 
