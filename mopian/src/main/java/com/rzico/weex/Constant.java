a609116710beb55161ade0a18dc9838bf06833fa
rzico <zhangsr@051204>
2018-06-20 14:05:48 +0800

    pine

--- mopian/src/main/java/com/rzico/weex/Constant.java
+++ mopian/src/main/java/com/rzico/weex/Constant.java
@@ -22,9 +22,9 @@ public class Constant {
     //这里的WEEX_CATEGORY 需要与配置文件中的意图配置统一： <category android:name="com.taobao.android.intent.category.YUNDIAN" />
     public static final String WEEX_CATEGORY = "com.taobao.android.intent.category.YUNDIAN";
 
-    public static final String wxURL = "http://cdnx.rzico.com/weex/app/";
+    public static final String wxURL = "http://cdnx.xmsdar.com/weex/app/";
 
-    public static final String resURL = "http://cdnx.rzico.com/";
+    public static final String resURL = "http://cdnx.xmsdar.com/";
 
     //    云店wxkey
     public static final String wxAppId = "wx490857e2baff7cfd";
@@ -50,8 +50,8 @@ public class Constant {
 
 
     public static String app = AllConstant.PACKNAME;
-    public static final String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-    public static final String bucket = "rzico-weex";
+    public static final String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
+    public static final String bucket = "sdar";
     public static final  String upLoadImages = "upload/images/";
     public static final  String upLoadVideos = "upload/videos/";
 
@@ -69,7 +69,7 @@ public class Constant {
 //    public static Map<String, WXSDKInstance> wxsdkInstanceMap;
 
     //生产环境服务器地址
-    public static final String SERVER = "http://weex.rzico.com/";
+    public static final String SERVER = "http://water.rzico.com/";
     public static final String PUBLIC_KEY =  "weex/common/public_key.jhtml";
     public static final String helperUrl = SERVER;
     public static boolean isLoginAcitivity = false;
