40de3012e20658051ed11c1315889a6e931c6fda
chenjinle <jinlesoft@163.com>
2018-06-27 14:56:59 +0800

    aaaa

--- mopian/src/main/java/com/rzico/weex/Constant.java
+++ mopian/src/main/java/com/rzico/weex/Constant.java
@@ -76,6 +76,6 @@ public class Constant {
 
     public static boolean isSetting = true; //设置权限
 
-    public static final int REQ_QR_CODE = 11002; // // 打开扫描界面请求码
+    public static final int REQ_QR_CODE = 11002; //打开扫描界面请求码
     public static final String INTENT_EXTRA_KEY_QR_SCAN = "qr_scan_result";
 }
