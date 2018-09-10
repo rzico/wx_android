9fdbdb5bc021c9d16eed203e5372e8fff0fe7ed5
rzico <zhangsr@051204>
2018-09-10 11:54:33 +0800

    fff

--- mopian/src/main/java/com/rzico/weex/Constant.java
+++ mopian/src/main/java/com/rzico/weex/Constant.java
@@ -75,9 +75,9 @@ public class Constant {
     public static boolean loginState = false;
     public static boolean unLinelogin = false;
     //当前程序的资源包版本
-    public static String resVerison = "1.3.5";
+    public static String resVerison = "1.3.6";
     //服务器资源包版本
-    public static String netResVerison = "1.3.5";
+    public static String netResVerison = "1.3.6";
     public static String appVerison = "";
 
 //    public static Map<String, WXSDKInstance> wxsdkInstanceMap;
